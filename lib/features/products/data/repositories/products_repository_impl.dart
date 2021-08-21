805840fb1720a10f6cec3142fad5be6af47ef0bb
Ali Dali <adali@redpiston.com>
2021-08-21 02:26:52 +0200

    working on products repo impl tests

--- lib/features/products/data/repositories/products_repository_impl.dart
+++ lib/features/products/data/repositories/products_repository_impl.dart
@@ -1,19 +1,33 @@
-import 'package:clementoni/features/products/domain/entities/products.dart';
-import 'package:clementoni/features/products/domain/entities/product.dart';
-import 'package:clementoni/core/error/failures.dart';
-import 'package:clementoni/features/products/domain/repositories/products_repository.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/network/internet_connection.dart';
+import '../../domain/entities/product.dart';
+import '../../domain/entities/products.dart';
+import '../../domain/repositories/products_repository.dart';
+import '../datasources/products_local_data_source.dart';
+import '../datasources/products_remote_data_source.dart';
+
 class ProductsRepositoryImpl extends ProductsRepository {
+  final ProductsRemoteDataSource remoteDataSource;
+  final ProductsLocalDataSource localDataSource;
+  final InternetConnection internetConnection;
+
+  ProductsRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.internetConnection,
+  });
+
   @override
-  Future<Either<Failure, Product>> getProduct(int id) {
-    // TODO: implement getProduct
-    throw UnimplementedError();
+  Future<Either<Failure, Products>> getProducts() async {
+    internetConnection.isConnected;
+    return Right(await remoteDataSource.getProducts());
   }
 
   @override
-  Future<Either<Failure, Products>> getProducts() {
-    // TODO: implement getProducts
+  Future<Either<Failure, Product>> getProduct(int id) {
+    // TODO: implement getProduct
     throw UnimplementedError();
   }
 }
---
+++ test/features/products/data/repositories/products_repository_impl_test.dart
@@ -0,0 +1,102 @@
+import 'package:clementoni/core/network/internet_connection.dart';
+import 'package:clementoni/features/products/data/datasources/products_local_data_source.dart';
+import 'package:clementoni/features/products/data/datasources/products_remote_data_source.dart';
+import 'package:clementoni/features/products/data/models/product_model.dart';
+import 'package:clementoni/features/products/data/models/products_model.dart';
+import 'package:clementoni/features/products/data/repositories/products_repository_impl.dart';
+import 'package:clementoni/features/products/domain/entities/product.dart';
+import 'package:clementoni/features/products/domain/entities/products.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'products_repository_impl_test.mocks.dart';
+
+@GenerateMocks([
+  ProductsRemoteDataSource,
+  ProductsLocalDataSource,
+  InternetConnection,
+])
+void main() {
+  final mockProductsRemoteDataSource = MockProductsRemoteDataSource();
+  final mockProductsLocalDataSource = MockProductsLocalDataSource();
+  final mockInternetConnection = MockInternetConnection();
+  final repositoryImpl = ProductsRepositoryImpl(
+    remoteDataSource: mockProductsRemoteDataSource,
+    localDataSource: mockProductsLocalDataSource,
+    internetConnection: mockInternetConnection,
+  );
+
+  final tProduct = Product(
+    id: 59122,
+    name: "RoboMaker Starter",
+    category: "Robot",
+    age: 8,
+    price: 45.95,
+    details: [
+      "Ein unglaubliches Labor, um die Geheimnisse der Robotik und Codierung durch Block-Programmierung zu entdecken.",
+      "Baue 3 Roboter aus mehr als 200 Bauteilen, 2 Motoren, 1 Lautsprecher und 1 IR Sensor, und programmiere ihn so, dass er lustige Aktivitäten ausführen kann, was zur Entwicklung des rechnerischen Denkens beiträgt.",
+      "Programmiere den X1-Drawer, um phantastische Designs zu erschaffen, spiel mit dem X2-Elevator, um Objekte aufzuheben oder schubse sie vom Tisch mit dem X3-Analyser!",
+      "Durch den Gebrauch der Bedienungsanleitung wirst du zum Programmier-Experten, der in der Lage ist, jede andere Art von Roboter zu bauen und so zu programmieren, dass sie alles tun, was du willst."
+    ],
+  );
+
+  final tProductModel = ProductModel(
+    id: 59122,
+    name: "RoboMaker Starter",
+    category: "Robot",
+    age: 8,
+    price: 45.95,
+    details: [
+      "Ein unglaubliches Labor, um die Geheimnisse der Robotik und Codierung durch Block-Programmierung zu entdecken.",
+      "Baue 3 Roboter aus mehr als 200 Bauteilen, 2 Motoren, 1 Lautsprecher und 1 IR Sensor, und programmiere ihn so, dass er lustige Aktivitäten ausführen kann, was zur Entwicklung des rechnerischen Denkens beiträgt.",
+      "Programmiere den X1-Drawer, um phantastische Designs zu erschaffen, spiel mit dem X2-Elevator, um Objekte aufzuheben oder schubse sie vom Tisch mit dem X3-Analyser!",
+      "Durch den Gebrauch der Bedienungsanleitung wirst du zum Programmier-Experten, der in der Lage ist, jede andere Art von Roboter zu bauen und so zu programmieren, dass sie alles tun, was du willst."
+    ],
+  );
+
+  final tProducts = Products(list: [tProduct]);
+  final tProductsModel = ProductsModel(list: [tProductModel]);
+
+  test('should check for internet connection', () async {
+    // arrange
+    when(mockInternetConnection.isConnected).thenAnswer((_) async => true);
+    when(mockProductsRemoteDataSource.getProducts())
+        .thenAnswer((_) async => tProductsModel);
+
+    // act
+    await repositoryImpl.getProducts();
+    // assert
+    verify(mockInternetConnection.isConnected);
+  });
+
+  group(
+    'device is online',
+    () {
+      setUp(() {
+        when(mockInternetConnection.isConnected).thenAnswer((_) async => true);
+      });
+
+      test('should return remote data', () async {
+        // arrange
+        when(mockProductsRemoteDataSource.getProducts())
+            .thenAnswer((_) async => tProductsModel);
+        // act
+        final products = await repositoryImpl.getProducts();
+        // assert
+        verify(mockProductsRemoteDataSource.getProducts());
+        expect(products, equals(Right(tProductsModel)));
+      });
+    },
+  );
+
+  group(
+    'device is offlie',
+    () {
+      setUp(() {
+        when(mockInternetConnection.isConnected).thenAnswer((_) async => false);
+      });
+    },
+  );
+}
